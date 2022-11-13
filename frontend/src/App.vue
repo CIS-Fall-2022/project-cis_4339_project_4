6e44dc42e243848fb3483711da1414e6156e0d65
Moises-Mora <moramoy@gmail.com>
2022-11-12 21:03:39 -0600

    Updated App.vue file to make an http request (by using axios) to an endpoint API to retrieve the name of the organization & pass the data to the template using the v-if directive.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,31 +9,51 @@
           <ul class="flex flex-col gap-4">
             <li>
               <router-link to="/">
-                <span style="position: relative; top: 6px" class="material-icons">dashboard</span>
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >dashboard</span
+                >
                 Dashboard
               </router-link>
             </li>
             <li>
               <router-link to="/intakeform">
-                <span style="position: relative; top: 6px" class="material-icons">people</span>
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >people</span
+                >
                 Client Intake Form
               </router-link>
             </li>
             <li>
               <router-link to="/eventform">
-                <span style="position: relative; top: 6px" class="material-icons">event</span>
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >event</span
+                >
                 Create Event
               </router-link>
             </li>
             <li>
               <router-link to="/findclient">
-                <span style="position: relative; top: 6px" class="material-icons">search</span>
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >search</span
+                >
                 Find Client
               </router-link>
             </li>
             <li>
               <router-link to="/findEvents">
-                <span style="position: relative; top: 6px" class="material-icons">search</span>
+                <span
+                  style="position: relative; top: 6px"
+                  class="material-icons"
+                  >search</span
+                >
                 Find Event
               </router-link>
             </li>
@@ -44,11 +64,30 @@
     <div class="grow w-4/5">
       <section
         class="justify-end items-center h-24 flex"
-        style="
-          background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%);
-        "
+        style="background: linear-gradient(250deg, #c8102e 70%, #efecec 50.6%)"
       >
-        <h1 class="mr-20 text-3xl text-white">Dataplatform</h1>
+        <h1 class="mr-20 text-3xl text-white" v-if="organization">
+          {{ organization }}
+        </h1>
+
+        <!-- Start of loading animation -->
+        <div class="mt-40" v-if="loading">
+          <p class="text-6xl font-bold text-center text-gray-500 animate-pulse">
+            Loading...
+          </p>
+        </div>
+        <!-- End of loading animation -->
+
+        <!-- Start of error alert -->
+        <div class="mt-12 bg-red-50" v-if="error">
+          <h3 class="px-4 py-1 text-4xl font-bold text-white bg-red-800">
+            {{ error.title }}
+          </h3>
+          <p class="p-4 text-lg font-bold text-red-900">
+            {{ error.message }}
+          </p>
+        </div>
+        <!-- End of error alert -->
       </section>
       <div>
         <router-view></router-view>
@@ -58,8 +97,58 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
+
+  data() {
+    return {
+      organization: "",
+      loading: false,
+      error: null,
+    };
+  },
+
+  methods: {
+    async fetchData() {
+      try {
+        this.error = null;
+        this.loading = true;
+        this.variable = null;
+        const url = `http://127.0.0.1:3000/organizationData/id/633dd0400b7c9d8f912fb0b2`;
+        const response = await axios.get(url);
+        //"re-organizing" - mapping json from the response
+        this.organization_name = response.data.map((item) => item.name);
+        this.organization = this.organization_name[0];
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.error = {
+            title: "Server Response",
+            message: err.message,
+          };
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.error = {
+            title: "Unable to Reach Server",
+            message: err.message,
+          };
+        } else {
+          // There's probably an error in your code
+          this.error = {
+            title: "Application Error",
+            message: err.message,
+          };
+        }
+      }
+      this.loading = false;
+    },
+  },
+
+  mounted() {
+    this.fetchData();
+  },
 };
 </script>
 

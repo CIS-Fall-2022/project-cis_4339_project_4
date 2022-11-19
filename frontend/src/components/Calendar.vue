e4990eae7a82d06ecd3032a709acf9b5110ea5e1
Creighton Worley <join0Rdie@protonmail.com>
2022-11-19 14:25:01 -0600

    Calendar working with event info from DB

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,8 +1,13 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
 <script>
 import '@fullcalendar/core/vdom' // solves problem with Vite
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import axios from 'axios';
 
 
 export default {
@@ -14,28 +19,30 @@ export default {
       calendarOptions: {
         plugins: [ dayGridPlugin, interactionPlugin ],
         initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
-        names: [],
-        dates: []
+        timeZone: 'America/Chicago',
+        events: []
       }
     }
   },
   methods: {
-
     async fetchData() {
-      const url = 'http://127.0.0.1:3000/eventData'
+      const url = 'http://localhost:3000/eventData'
       const response = await axios.get(url);
-      this.names = response.data.map((item) => item.eventData.eventName);
-      this.dates = response.data.map((item) => item.eventData.date);
+      var returnedData = response.data
+      var arr = [];
+      returnedData.forEach(data => {
+        var payload = {
+          title: data.eventName,
+          date: data.date
+        }
+        arr.push(payload)
+      });
+      this.calendarOptions.events = arr
+    },
     
   },
-
   mounted() {
     this.fetchData();
   }
 }
-}
 </script>
-<template>
-  <FullCalendar :options="calendarOptions" />
-</template>
